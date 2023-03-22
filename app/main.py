736953640d152ab880f308f14fda009ce2a50c72
Mangkorn <76460553+KyosaixZ@users.noreply.github.com>
2023-03-22 14:22:29 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,16 +3,8 @@ from mangum import Mangum
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "Mangkorn"}
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
 @app.get("/callname/{name}")
-def read_name(name: str = None):
+def read_name(name: str):
     return {"hello": name}
 
 @app.post("/callname")
